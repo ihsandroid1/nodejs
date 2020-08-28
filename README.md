d7b5c9f2d3f61900fa53a7c04f87d7902b1d9f87
ihsandroid1 <maenbos30@gmail.com>
2020-08-29 02:43:57 +0700

    commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Belajar Node JS
-- Tes Penanganan Form
-- Method GET & POST yang sederhana
 - Tes port 3001
 - Kill Port : fuser -n tcp -k 3001 
--- server.js
+++ server.js
@@ -1,45 +1,11 @@
 const http = require('http');
-const qs = require('querystring');
+const fs = require('fs');
 
-const server =  http.createServer(function(req,res) {
+let html = fs.readFileSync('./index.html');
+
+let server =  http.createServer(function(req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-
-    if (req.url=='/'){
-        switch (req.method){
-            case 'GET':
-                res.end(
-                    `<h2>Demo Penanganan Form</h2>
-                    <form action="/" method="post">
-                    <label>Nama:<label><br>
-                    <input type="text" name="nama"><br><br>
-                    <label>Email:</label><br>
-                    <input type="email" name="email"><br><br>
-                    <input type="submit" name="btnSubmit" value="Kirim">
-                    </form>`
-                );
-                break;
-            case 'POST':
-                let body ='';
-                req.on('data', function(data){
-                    body += data;
-                });
-
-                req.on('end', function (){
-                    let form = qs.parse(body);
-                    res.end(`
-                        <strong>Data yang dikirim</strong>
-                        <br><br>
-                        Nama: ${form['nama']}<br>
-                        Email: ${form['email']}
-                        `
-                    );
-                });
-                break;
-            default:
-                res.end('Metode pengiriman tidak sah');
-        }
-    }
- 
+    res.end(html);
 });
 
 server.listen(3001);
